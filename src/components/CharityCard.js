baedb7c3d7b5861b7b75bbcf992ef17668c7b1d9
amritesh09 <amritesh.srivastava@booking.com>
2020-06-30 19:30:12 +0200

    added path for India

--- src/components/CharityCard.js
+++ src/components/CharityCard.js
@@ -10,20 +10,38 @@ shouldComponentUpdate( nextProps, nextState ){
 		return(
 			<div className="card-deck">
 				<div className="card mb-4">
-					<div className="card-body">
-				      {charity.fields.Organization && <h2 className="card-text mb-4">{charity.fields.Organization}</h2>}
-				      {charity.fields.Where && <p className="card-text"><b>Where</b>: {charity.fields.Where.join(', ') }</p>}
-				      {charity.fields.What && <p className="card-text"><b>What</b>: {charity.fields.What.join(', ') }</p>}
-				      {charity.fields.Package && <p className="card-text"><b>Package</b>: {charity.fields.Package }</p>}
-				      <p className="card-text"><b>Deliveries per week</b>: {charity.fields["How many"] }</p>
-				      {charity.fields.Fundraising && <p className="card-text"><b>Fundraising</b>: {charity.fields.Fundraising.join(', ') }</p>}
-				      <p className="card-text"><b>Contact Name</b>: {charity.fields["Contact name"] }</p>
-				      <p className="card-text">
-					      {charity.fields.Email && <a href={"mailto:"+charity.fields.Email} className="btn btn-info mr-3">EMAIL</a>}
-					      {charity.fields.FB && <a href={charity.fields.FB} className="btn btn-primary mr-3" target="_blank">FB</a>}
-					      {charity.fields.WWW && <a href={charity.fields.WWW} className="btn btn-success" target="_blank">WWW</a>}
-				      </p>
-		      	</div>
+					{this.props.location && this.props.location == "in" ?
+						<div className="card-body">
+					      {charity.fields.Organization && <h2 className="card-text mb-4">{charity.fields.Organization}</h2>}
+					      {charity.fields.Where && <p className="card-text"><b>Where</b>: {charity.fields.Where.join(', ') }</p>}
+					      {charity.fields.What && <p className="card-text"><b>What</b>: {charity.fields.What.join(', ') }</p>}
+					      {charity.fields.Package && <p className="card-text"><b>Package</b>: {charity.fields.Package }</p>}
+					      <p className="card-text"><b>Deliveries per week</b>: {charity.fields["How many"] }</p>
+					      {charity.fields.Fundraising && <p className="card-text"><b>Fundraising</b>: {charity.fields.Fundraising.join(', ') }</p>}
+					      <p className="card-text"><b>Contact Name</b>: {charity.fields["Contact name"] }</p>
+					      <p className="card-text">
+						      {charity.fields.Email && <a href={"mailto:"+charity.fields.Email} className="btn btn-info mr-3">EMAIL</a>}
+						      {charity.fields.FB && <a href={charity.fields.FB} className="btn btn-primary mr-3" target="_blank">FB</a>}
+						      {charity.fields.WWW && <a href={charity.fields.WWW} className="btn btn-success" target="_blank">WWW</a>}
+					      </p>
+						}
+			      	</div> :
+			      		<div className="card-body">
+					      {charity.fields.Organization && <h2 className="card-text mb-4">{charity.fields.Organization}</h2>}
+					      {charity.fields.Where && <p className="card-text"><b>Where</b>: {charity.fields.Where.join(', ') }</p>}
+					      {charity.fields.Type && <p className="card-text"><b>Type</b>: {charity.fields.Type.join(', ') }</p>}
+					      {charity.fields.Description && <p className="card-text"><b>Description</b>: {charity.fields.Description }</p>}
+					      {charity.fields.Fundraising && <p className="card-text"><b>Fundraising</b>: {charity.fields.Fundraising.join(', ') }</p>}
+					      {charity.fields["ContactName"] && <p className="card-text"><b>Contact Name</b>: {charity.fields["ContactName"] }</p> }
+					      {charity.fields["ContactNumber"] && <p className="card-text"><b>Contact Number</b>: {charity.fields["ContactNumber"] }</p>}
+					      <p className="card-text">
+						      {charity.fields.Email && <a href={"mailto:"+charity.fields.Email} className="btn btn-info mr-3">EMAIL</a>}
+						      {charity.fields.FB && <a href={charity.fields.FB} className="btn btn-primary mr-3" target="_blank">FB</a>}
+						      {charity.fields.WWW && <a href={charity.fields.WWW} className="btn btn-success" target="_blank">WWW</a>}
+					      </p>
+						
+			      	</div>
+			      	}
 		      </div>
 		      </div>
 		);
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,20 +1,39 @@
 import React, { Component } from 'react';
 import MapAndCharities from './MapAndCharities';
+import MapAndCharitiesIn from './MapAndCharitiesIn';
 
 class Home extends Component {
 
 	render() {
 		const queryString = require('query-string');
 		var parsed = queryString.parse(this.props.location.search);
+		var lat = '-8.2238968';
+		var lng = '114.9516869';
+		// if(parsed.location && parsed.location == "in") {
+		// 	lat = '19.0760';
+		// 	lng = '72.8777';
+		// }
 		return(
 			<div style={{}}>
-				<MapAndCharities
+				{parsed.location && parsed.location == "in" ?
+				<MapAndCharitiesIn
 					google={this.props.google}
-					center={{lat: -8.2238968, lng: 114.9516869}}
+					center={{lat: 19.0760, lng: 72.8777}}
 					height='100vh'
 					zoom={9}
 					tab={parsed.tab}
+					location={parsed.location}
+				/> : 
+				<MapAndCharities
+					google={this.props.google}
+					center={{lat: lat, lng: lng}}
+					height='100vh'
+					zoom={9}
+					tab={parsed.tab}
+					location={parsed.location}
 				/>
+				}
+				
 			</div>
 		);
 	}
